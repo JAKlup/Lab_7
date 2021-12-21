519815d03d4961eb91b8e9eb0250c90f2163156c
JAKlup <90199477+JAKlup@users.noreply.github.com>
2021-12-21 09:34:31 +0300

    Add files via upload

---
+++ Lab_7/Task_2/Program_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_2
+{
+    internal class Program_2
+    {
+        static void Main(string[] args)
+        {
+            String str = Console.ReadLine();
+            String modifiedStr = new String(str.Select(s => Char.IsLetter(s) ? '*' : s).ToArray());
+            int punctuationCount = str.Count(s => Char.IsPunctuation(s));
+
+            Console.WriteLine("Измененная строка: " + modifiedStr);
+            Console.WriteLine("Количество знаков препинания: " + punctuationCount);
+        }
+    }
+}
