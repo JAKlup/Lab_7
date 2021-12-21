89a0adc43337f9cf53a81303e8b2aa656e491ddf
JAKlup <90199477+JAKlup@users.noreply.github.com>
2021-12-21 09:29:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] str = Console.ReadLine().ToCharArray();
+            int punctuationCount = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsPunctuation(str[i]))
+                {
+                    punctuationCount++;
+                }
+                if (Char.IsLetter(str[i]))
+                {
+                    str[i] = '*';
+                }
+            }
+            Console.Write("Измененная строка: ");
+            Console.WriteLine(str);
+            Console.WriteLine("Количество знаков препинания: " + punctuationCount);
+        }
+    }
+}
